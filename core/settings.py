20e03832b6e50f639a325b11361c9dcffe7e32e2
dalbanhi <dalbanhi@gmail.com>
2024-03-26 18:50:01 -0500

    Vercel hosting

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-0sq&u0eokv71$a3snu5#g=^cxrb=)1$8@ck#nz*8ird_ezj_vi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'localhost', ]
 
 
 # Application definition
@@ -125,7 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = [BASE_DIR / "static",]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # Default primary key field type
