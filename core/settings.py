dfa24ff619ed85f320353a74ba0094ab786ed3fa
dalbanhi <dalbanhi@gmail.com>
2024-04-02 20:54:00 -0500

    added whitenoise and collectstatic compressed files

--- core/settings.py
+++ core/settings.py
@@ -133,9 +133,14 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static",]
+
+STORAGES = {
+    "staticfiles": {
+        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+    }
+}
 
 
 
