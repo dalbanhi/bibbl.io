38447111eee9cdc75c669d4075c5f0f42baebddf
dalbanhi <dalbanhi@gmail.com>
2024-04-03 18:48:50 -0500

    added csrf token cookie getter

--- assets/js/components/util/cookies/CSRFToken.js
+++ assets/js/components/util/cookies/CSRFToken.js
@@ -4,11 +4,27 @@ import Cookies from 'js-cookie';
 let csrftoken = Cookies.get('csrftoken');
 
 const CSRFToken = () => {
+
+  function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+      const cookies = document.cookie.split(';');
+      for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        if (cookie.substring(0, name.length + 1) === (name + '=')) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
+    return cookieValue;
+  }
   
   return (
     <>
       {console.log("token here", csrftoken)}
-      <input type="hidden" name='csrfmiddlewaretoken' value={csrftoken}></input>
+      {console.log("token here", getCookie("csrftoken"))}
+      <input type="hidden" name='csrfmiddlewaretoken' value={getCookie("csrftoken")}></input>
     </>
     
   )
